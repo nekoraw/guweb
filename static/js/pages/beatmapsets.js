4e8811e950bfca7980f1074af4ea45ed622a7087
luis10barbo <luis10barbo2020@gmail.com>
2022-11-19 15:25:23 -0300

    Removed ParseScoreTime function

--- static/js/pages/beatmapsets.js
+++ static/js/pages/beatmapsets.js
@@ -33,35 +33,6 @@ new Vue({
         this.$set(this, "extraMode", this.ModeStrToInt(extraMode, true));
         // console.log(this.bmsId, this.mode, this.bmId);
       },
-      ParseScoreTime(dateTimeString) {
-        const date = new Date(dateTimeString)
-        var seconds = Math.floor((new Date() - date) / 1000);
-
-        var interval = seconds / 31536000;
-
-        if (interval > 1) {
-          return Math.floor(interval) + " anos";
-        }
-        interval = seconds / 2592000;
-        if (interval > 1) {
-          return Math.floor(interval) + " meses";
-        }
-        interval = seconds / 86400;
-        if (interval > 1) {
-          return Math.floor(interval) + " dias";
-        }
-        interval = seconds / 3600;
-        if (interval > 1) {
-          return Math.floor(interval) + " horas";
-        }
-        interval = seconds / 60;
-        if (interval > 1) {
-          return Math.floor(interval) + " minutos";
-        }
-        return Math.floor(seconds) + " segundos";
-
-      }
-      ,
       ChangeSort(sort) {
         if (this.sort === sort) return;
         this.$set(this, "sort", sort);
